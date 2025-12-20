e4c6dc7ca296ad7e12e59a5669893fd158732cbb
GOVIND <work.govindsahu@gmail.com>
2025-12-20 14:00:32 +0530

    produciton-change-13

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -73,19 +73,12 @@ export const handleGitHubWebhook = async (req, res, next) => {
 
     const repositoryName = req.body.repository.name;
 
-    console.log(repositoryName);
-    console.log(req.body);
-
     const bashFile =
       repositoryName === "Storage-Frontend"
         ? "deploy-frontend-ec2.sh"
         : "deploy-backend.sh";
 
-    console.log(bashFile);
-
-    const bashChildProcess = spawn("bash", [
-      "/home/ubuntu/deploy-frontend-ec2.sh",
-    ]);
+    const bashChildProcess = spawn("bash", [`/home/ubuntu/${bashFile}`]);
 
     bashChildProcess.stdout.on("data", (data) => {
       process.stdout.write(data);
