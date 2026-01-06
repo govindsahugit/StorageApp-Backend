b08ce880fd2170c3a8829d98d6a2960a76de7bb4
GOVIND <work.govindsahu@gmail.com>
2026-01-06 11:56:43 +0530

    increase file size

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const port = 4000;
 
 const whitelist = [
   process.env.CLIENT_URL,
-  "https://app.govindsahu.me",
   "https://storageapp.govindsahu.me",
+  "http://localhost:5173",
 ];
 
 app.use(
@@ -43,9 +43,7 @@ app.use(helmet());
 
 app.use(cookieParser(process.env.COOKIE_PARSER_SESSION_KEY));
 
-// Increase payload size limits for file uploads
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(express.json());
 
 app.use(express.static("storage"));
 
